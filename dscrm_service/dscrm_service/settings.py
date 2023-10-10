74aa5ac6f546200c56e89b0dbd15ded5ed13d5da
Gevorg <gevorg.ar.23@gmail.com>
2023-10-10 16:23:05 +0300

    add logging

--- dscrm_service/dscrm_service/settings.py
+++ dscrm_service/dscrm_service/settings.py
@@ -136,3 +136,29 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters':{
+        'main_format': {
+            'format': '{asctime} - {levelname} - {module} - {message}',
+            'style': '{',
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'WARNING',
+            'class': 'logging.FileHandler',
+            'filename': BASE_DIR / 'warning.log',
+            'formatter': 'main_format'
+        },
+    },
+    'loggers': {
+        'main': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
